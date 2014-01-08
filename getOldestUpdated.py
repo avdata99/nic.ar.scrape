5770bdde22c458e160badc9c7b6b545cdda087ad
avdata99 <andres@data99.com.ar>
2014-01-08 20:16:31 -0300

    agregar lector de dominios a scrapear de servidor externo

---
+++ getOldestUpdated.py
@@ -0,0 +1,49 @@
+# obtener los dominios menos actualizados via api y procesarlos
+# la cantidad de dominios se puede especificar via sys.argv[1]
+
+import json
+import urllib2
+import time
+import os
+from random import randint
+import sys
+import ConfigParser
+
+here = os.path.dirname(os.path.abspath(__file__))
+Config = ConfigParser.ConfigParser()
+Config.read(os.path.join(here,"config.ini"))
+
+print "BUSCANDO \n"
+tot = 1
+if len(sys.argv) > 0:
+    tot = sys.argv[1]
+
+url = Config.get("servers", 'readDomainsFrom')
+url = url % tot
+print url
+data = json.load(urllib2.urlopen(url))
+
+print "ENCONTRADO \n"
+print data
+
+print "RESULTADOS \n"
+print data["result"]
+
+c = 0
+for f in data["result"]:
+    c = c + 1
+    print "\nDATA\n %s" % f
+    print "OK %s lastUpdate %s" % (f["dominio"], f["lastUpdated"])
+    dom = f["dominio"].replace(".com.ar", "").encode("latin-1")
+    print "\n******\n %s \n*******\n" % dom
+
+    cmd = "python %snicarscrape.py %s send" % (Config.get("servers", "scraperFolder"), dom)
+    cmd = unicode(cmd)
+    print cmd
+    test = os.system(cmd)
+
+    print "\nprocesando %i de %s" % (c, tot)
+    if c < int(tot):
+        r = randint(38, 49)
+        print "\n ---- %s ---- \n" % r
+        time.sleep(r)
